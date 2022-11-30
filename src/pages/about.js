08f2a12c4d3a226ebce36a36cd72d6c9ff6819d5
ilhamdaffa21 <imdaffa2001@gmail.com>
2022-11-30 23:58:43 +0700

    Update about.js

--- src/pages/about.js
+++ src/pages/about.js
@@ -6,33 +6,7 @@ export default function About() {
   const theme = useContext(ThemeContext);
   return (
     <Layout>
-      <div
-        className={
-          theme.theme === 'light'
-            ? 'flex items-center flex-col mb-20 p-10 font-semibold text-lg'
-            : 'flex items-center flex-col mb-20 p-10 text-white font-semibold text-lg'
-        }
-      >
-        <p>
-          This website is made for learning purpose. I made this website using
-          ReactJS, TailwindCSS, and React Router.
-        </p>
-        <p>Software Engineering Practice Final Project by group 16.</p>
-        <p>Made by :</p>
-        <ul className="flex flex-col items-center pt-10">
-          <li>
-            <a href="https://instagram.com/ahmad_rafly_">Ahmad Rafly Pradana</a>
-          </li>
-          <li>
-            <a href="https://instagram.com/imdaffa21">Ilham Muhammad Daffa</a>
-          </li>
-          <li>
-            <a href="https://instagram.com/oddyramadhanp">
-              Oddy Ramadhan Putra Winata
-            </a>
-          </li>
-        </ul>
-      </div>
+
     </Layout>
   );
 }
